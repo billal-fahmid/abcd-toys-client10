777d9de958ad3ba341844a491cc785dde89a6e35
billal-fahmid <billalcooom@gmail.com>
2023-05-18 08:56:21 +0600

    basic site setup with react route and navbar

---
+++ src/pages/MyToys/MyToys.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyToys = () => {
+    return (
+        <div>
+            <h3>my toys page</h3>
+        </div>
+    );
+};
+
+export default MyToys;
---
+++ src/routes/Route.jsx
@@ -0,0 +1,38 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/Home/Home/Home";
+import AllToys from "../pages/AllToys/AllToys";
+import MyToys from "../pages/MyToys/MyToys";
+import AddToy from "../pages/AddToy/AddToy";
+import Blogs from "../pages/Blogs/Blogs";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout></MainLayout>,
+      children:[
+        {
+            path:'/',
+            element:<Home></Home>
+        },
+        {
+            path:'allToys',
+            element:<AllToys></AllToys>
+        },
+        {
+            path:'myToys',
+            element:<MyToys></MyToys>
+        },
+        {
+            path:'addToy',
+            element:<AddToy></AddToy>
+        },
+        {
+            path:'blogs',
+            element:<Blogs></Blogs>
+        }
+      ]
+    },
+  ]);
+
+  export default router;
