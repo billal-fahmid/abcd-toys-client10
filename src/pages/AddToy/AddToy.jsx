e9841347e30eb06848504a92e9d7459364884e27
billal-fahmid <billalcooom@gmail.com>
2023-05-18 16:27:27 +0600

    add toy page input form by get data in object

--- src/pages/AddToy/AddToy.jsx
+++ src/pages/AddToy/AddToy.jsx
@@ -1,93 +1,112 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useForm } from "react-hook-form";
+import { AuthContext } from '../../provider/AuthProvider';
 
 const AddToy = () => {
+    const { user } = useContext(AuthContext)
 
     const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+    const onSubmit = data => {
+        console.log(data)
+        fetch('')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+    };
 
     return (
-        <div>
+        <div className='w-full p-16 bg-pink-200'>
 
-            <form onSubmit={handleSubmit(onSubmit)}>
-             
+            <div className='text-center'>
+                <h3 className='text-3xl font-bold pb-4'>Add Toy</h3>
+               
+            </div>
+
+            <form className='w-3/4 mx-auto' onSubmit={handleSubmit(onSubmit)}>
+
                 <div className="mb-2">
-                  
+
                     <input
                         {...register("photoURL")}
                         type="text"
-                        
+                        placeholder='Photo URL of Toy'
                         className="block w-full px-4 py-2 mt-2 text-pink-700 bg-white border rounded-md focus:border-pink-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
                     />
                 </div>
                 <div className="mb-2">
-                  
+
                     <input
                         {...register("name")}
                         type="text"
-                        
+                        placeholder='Toy Name'
                         className="block w-full px-4 py-2 mt-2 text-pink-700 bg-white border rounded-md focus:border-pink-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
                     />
                 </div>
                 <div className="mb-2">
-                  
+
                     <input
                         {...register("sellerName")}
                         type="text"
-                        
+                        placeholder='Seller Name'
+                        defaultValue={user?.displayName}
                         className="block w-full px-4 py-2 mt-2 text-pink-700 bg-white border rounded-md focus:border-pink-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
                     />
                 </div>
                 <div className="mb-2">
-                  
+
                     <input
                         {...register("sellerEmail")}
                         type="text"
-                        
+                        placeholder='Seller Email'
+                        defaultValue={user?.email}
                         className="block w-full px-4 py-2 mt-2 text-pink-700 bg-white border rounded-md focus:border-pink-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
                     />
                 </div>
+                <div className='mb-2'>
+                    <select placeholder='Sub Category' className="block w-full px-4 py-2 mt-2 text-pink-700 bg-white border rounded-md focus:border-pink-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40" {...register("subCategory")}>
+                        <option value="female">Math Toys</option>
+                        <option value="male">Engineering Toys</option>
+                        <option value="other">Physicis Toys</option>
+                    </select>
+                </div>
                 <div className="mb-2">
-                  
+
                     <input
                         {...register("price")}
                         type="text"
-                        
+                        placeholder='Price'
                         className="block w-full px-4 py-2 mt-2 text-pink-700 bg-white border rounded-md focus:border-pink-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
                     />
                 </div>
                 <div className="mb-2">
-                  
+
                     <input
                         {...register("rating")}
                         type="text"
-                        
+                        placeholder='Rating'
                         className="block w-full px-4 py-2 mt-2 text-pink-700 bg-white border rounded-md focus:border-pink-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
                     />
                 </div>
                 <div className="mb-2">
-                  
+
                     <input
                         {...register("quantity")}
                         type="text"
-                        
+                        placeholder='Available Quantity'
                         className="block w-full px-4 py-2 mt-2 text-pink-700 bg-white border rounded-md focus:border-pink-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
                     />
                 </div>
                 <div className="mb-2">
-                  
+
                     <input
                         {...register("description")}
                         type="text"
-                        
+                        placeholder='Details Descriptions'
                         className="block w-full px-4 py-2 mt-2 text-pink-700 bg-white border rounded-md focus:border-pink-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
                     />
                 </div>
-                {/* <select {...register("gender")}>
-                    <option value="female">female</option>
-                    <option value="male">male</option>
-                    <option value="other">other</option>
-                </select> */}
+
                 <input type="submit" />
             </form>
             {/* <form onSubmit={handleSubmit(onSubmit)}>
