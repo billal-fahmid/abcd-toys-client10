6a69826e73c33379972691efe7416576acf4385a
billal-fahmid <billalcooom@gmail.com>
2023-05-19 10:42:38 +0600

    details page done

--- src/pages/toyDetails/toyDetails.jsx
+++ src/pages/toyDetails/toyDetails.jsx
@@ -1,22 +1,37 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useLoaderData } from 'react-router-dom';
+
+import { Rating } from '@smastrom/react-rating'
+import '@smastrom/react-rating/style.css'
+
 
 const ToyDetails = () => {
+
+
+
     const details = useLoaderData()
     console.log(details)
     return (
         <div className='py-24 px-14'>
-              <div className='text-center'>
+      
+            <div className='text-center'>
                 <h3 className='text-3xl font-bold pb-4'> Discover Amazing Toy Details</h3>
 
             </div>
+
             <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col lg:flex-row-reverse">
-                    <img src="/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+                <div className="hero-content flex-col lg:flex-row gap-24">
+                    <img src={details?.photoURL} className="max-w-sm rounded-lg shadow-2xl" />
                     <div>
-                        <h1 className="text-5xl font-bold">Box Office News!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                       
+                        <h1 className="text-3xl font-bold">Toy Name: {details?.name}</h1>
+                        <p className="pt-5 text-xl">Seller Name: {details.sellerName ? details.sellerName : 'Not Available'}</p>
+                        <p className="pt-5 text-xl">Seller Email: {details.sellerEmail ? details.sellerEmail : 'Not Available'}</p>
+                        <p className="pt-5 text-xl font-semibold">Price: {details?.price} $</p>
+                        <p className='flex text-xl items-center pt-4'>Rating : {details?.rating} <span className='ml-5 '><Rating style={{ maxWidth: 100 }} value={details?.rating} readOnly /></span></p>
+                        <p className="pt-5 text-xl ">Available Quantity: {details?.quantity}</p>
+                        <p className="pt-5 text-xl mb-10 ">Description: {details?.description}</p>
+                        <Link to='/allToys' className='px-8 py-3  text-white font-bold  bg-pink-500 border-none'>Back</Link>
+
                     </div>
                 </div>
             </div>
